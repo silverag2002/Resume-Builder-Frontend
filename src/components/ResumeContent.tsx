174d7a76416f3685f2dfa649ec9f36e75831a069
silverag2002 <silverag2002@gmail.com>
2023-05-12 01:05:56 +0530

    Animation added

--- src/components/ResumeContent.tsx
+++ src/components/ResumeContent.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 
 const ResumeContent = () => {
+  const [showPersonalDetails, setShowPersonalDetails] = useState(false);
+
+  function handleClick(event: any) {
+    const id = event.target.getAttribute("id");
+    console.log(id);
+    setShowPersonalDetails(!showPersonalDetails);
+  }
   return (
     <div className="flex flex-row">
-      <div className="w-1/2 px-24 py-16 h-auto">
+      <div className="w-1/2 px-24 py-16 h-auto overflow-y-hidden overflow-x-scroll">
         <div className="bg-violet-600 rounded-full px-2 py-2 w-10  text-white ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -20,34 +27,42 @@ const ResumeContent = () => {
             />
           </svg>
         </div>
-        <div className="font-semibold text-3xl my-6 ">Personal details</div>
-        <div className="flex flex-col">
-          <div className="flex flex-row items-center justify-between my-4">
-            <div className="flex flex-col w-10/12">
-              <span className="text-sm text-gray-400">Job Title</span>
-              <input
-                type="text"
-                name="jobTitle"
-                className="bg-gray-200 h-12 px-4 outline-none rounded-lg flex-2 mr-4"
+        <div
+          className="font-semibold text-3xl my-6 flex flex-row justify-between items-center cursor-pointer "
+          onClick={(event) => handleClick(event)}
+          id="personal-details"
+        >
+          <span id="personal-details">Personal details</span>{" "}
+          <div className="border-x border-y rounded-md border-violet-600 p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className={`w-6 h-6 text-violet-600  transition-all duration-500 ease-in-out ${
+                showPersonalDetails ? "rotate-180" : ""
+              }`}
+              id="personal-details"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4.5 15.75l7.5-7.5 7.5 7.5"
               />
-            </div>
-            <button className="w-auto  bg-gray-200 rounded-lg p-6">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6  "
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
-                />
-              </svg>
-            </button>
+            </svg>
           </div>
+        </div>
+        <div
+          className={`w-full h-1 ${
+            showPersonalDetails ? "hidden" : ""
+          } bg-gray-200`}
+        ></div>
+        <div
+          className={` rounded-md  transition-all duration-500 max-h-0 overflow-hidden ${
+            showPersonalDetails ? "max-h-full p-8 border-2" : ""
+          }`}
+        >
           <div className="flex flex-row items-center justify-between my-4">
             <div className="flex flex-col w-1/2 mr-4">
               <span className="text-sm text-gray-400">First Name</span>
@@ -85,8 +100,28 @@ const ResumeContent = () => {
             </div>
           </div>
           <div className="flex flex-row items-center justify-between my-4">
+            <div className="flex flex-col w-full ">
+              <span className="text-sm text-gray-400">Headline</span>
+              <input
+                type="text"
+                name="jobTitle"
+                className="bg-gray-200 h-12 px-4 outline-none rounded-lg flex-2 "
+              />
+            </div>
+          </div>
+          <div className="flex flex-row items-center justify-between my-4">
+            <div className="flex flex-col w-full ">
+              <span className="text-sm text-gray-400">Address</span>
+              <input
+                type="text"
+                name="jobTitle"
+                className="bg-gray-200 h-12 px-4 outline-none rounded-lg flex-2 "
+              />
+            </div>
+          </div>
+          <div className="flex flex-row items-center justify-between my-4">
             <div className="flex flex-col w-1/2 mr-4">
-              <span className="text-sm text-gray-400">Country</span>
+              <span className="text-sm text-gray-400">City</span>
               <input
                 type="text"
                 name="jobTitle"
@@ -94,7 +129,7 @@ const ResumeContent = () => {
               />
             </div>
             <div className="flex flex-col w-1/2 ml-4">
-              <span className="text-sm text-gray-400 ">City</span>
+              <span className="text-sm text-gray-400 ">Country</span>
               <input
                 type="text"
                 name="jobTitle"
@@ -106,7 +141,7 @@ const ResumeContent = () => {
             <span>Add more details +</span>
           </div>
         </div>
-        <div className="font-semibold text-3xl my-6 ">
+        {/* <div className="font-semibold text-3xl my-6 ">
           <span>Professional Summary</span>
           <button className="w-auto mx-4">
             <svg
@@ -124,9 +159,9 @@ const ResumeContent = () => {
               />
             </svg>
           </button>
-        </div>
+        </div> */}
 
-        <div className="flex flex-row items-center justify-between my-4">
+        {/* <div className="flex flex-row items-center justify-between my-4">
           <div className="flex flex-col w-full h-3/4 mr-4">
             <span className="text-sm text-gray-400">
               A short description about your professional career
@@ -136,7 +171,7 @@ const ResumeContent = () => {
               className="bg-gray-200 h-64 px-4 outline-none rounded-lg flex-2 resize-none row-start-5"
             />
           </div>
-        </div>
+        </div> */}
         <div className="font-semibold text-3xl my-6 ">
           <span>Employment History</span>
           <button className="w-auto mx-4">
@@ -155,6 +190,85 @@ const ResumeContent = () => {
               />
             </svg>
           </button>
+        </div>
+        <div className="flex flex-col border-2 rounded-md p-8">
+          <div className="flex flex-row justify-between">
+            <span className="text-lg">School</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 text-violet-600"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4.5 15.75l7.5-7.5 7.5 7.5"
+              />
+            </svg>
+          </div>
+          <div className="flex flex-row items-center justify-between my-4 w-full">
+            <div className="flex flex-col w-4/6">
+              <span className="text-sm text-gray-400">Employer</span>
+              <input
+                type="text"
+                name="jobTitle"
+                className="bg-gray-200 h-12 px-4 outline-none rounded-lg flex-2 "
+              />
+            </div>
+            <div className="flex flex-col  ml-4">
+              <span className="text-sm text-gray-400 ">Position</span>
+              <input
+                type="text"
+                name="jobTitle"
+                className="bg-gray-200 h-12 px-4 outline-none rounded-lg flex-2 "
+              />
+            </div>
+          </div>
+          <div className="flex flex-row items-center justify-between my-4">
+            <div className="flex flex-col w-7/12 mr-3">
+              <div className="flex flex-row justify-between items-center">
+                <span className="text-sm text-gray-400">
+                  Start and End date
+                </span>
+                <div className="flex flex-row">
+                  <input type="checkbox" name="eduPresent" />
+                  <span className="text-sm text-black mx-1">Present</span>
+                </div>
+              </div>
+              <div className="flex flex-row justify-between items-center my-0.5">
+                <input
+                  type="month"
+                  name="jobTitle"
+                  className="bg-gray-200 h-12 px-4 pr-3 outline-none rounded-lg flex-2 mr-1"
+                />
+                <input
+                  type="month"
+                  name="jobTitle"
+                  className="bg-gray-200 h-12 px-4 pr-3 outline-none rounded-lg flex-2 ml-1"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col w-4/12 ml-4">
+              <span className="text-sm text-gray-400 ">City</span>
+              <input
+                type="text"
+                name="jobTitle"
+                className="bg-gray-200 h-12 px-4 outline-none rounded-lg flex-2 "
+              />
+            </div>
+          </div>
+          <div className="flex flex-row items-center justify-between my-4">
+            <div className="flex flex-col w-full h-3/4 ">
+              <span className="text-sm text-gray-400">Description</span>
+              <textarea
+                name="jobTitle"
+                className="bg-gray-200 h-64 p-4 pr-0 outline-none rounded-lg flex-2 resize-none row-start-5"
+              />
+            </div>
+          </div>
         </div>
         <div className="text-violet-600 flex flex-row justify-start items-center">
           <span>Add Employment +</span>
@@ -401,7 +515,7 @@ const ResumeContent = () => {
         </div>
       </div>
 
-      <div className="w-1/2 h-screen  bg-gray-200 flex flex-col items-center ">
+      <div className="w-1/2 h-screen  bg-gray-200 flex flex-col items-center fixed right-0">
         <div className="bg-white text-black m-20 mb-6 h-3/4 w-3/4 "></div>
         <div className="flex flex-row justify-between items-center w-3/4 my-2">
           <button className="text-violet-600 ">Select template</button>
